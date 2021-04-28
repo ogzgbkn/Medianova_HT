789d7b1fe678ad93d226dcc79437c525226d779b
Oğuz Gögebakan <43935167+ogzgbkn@users.noreply.github.com>
2021-04-28 18:34:12 +0300

    dealerHandCheck() comments are added.

--- index.php
+++ index.php
@@ -11,17 +11,3 @@
     $_SESSION['myGame'] = $myGame;
 
 ?>
-
-<!--<!DOCTYPE html>
-<html>
-<body>
-
-    <form action="hit.php" method="get">
-        <input type="submit" value="Hit">
-    </form>
-    <form action="stay.php" method="get">
-        <input type="submit" value="Stay">
-    </form>
-
-</body>
-</html>-->
